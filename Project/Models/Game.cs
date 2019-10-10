6bedb508c95ab494209ab68a61397db4e7b37ca0
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-10-09 21:00:04 -0600

    init

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -2,15 +2,15 @@ using ConsoleAdventure.Project.Interfaces;
 
 namespace ConsoleAdventure.Project.Models
 {
-    public class Game : IGame
-    {
-        public IRoom CurrentRoom { get; set; }
-        public IPlayer CurrentPlayer { get; set; }
+  public class Game : IGame
+  {
+    public IRoom CurrentRoom { get; set; }
+    public IPlayer CurrentPlayer { get; set; }
 
-        //NOTE Make yo rooms here...
-        public void Setup()
-        {
-            throw new System.NotImplementedException();
-        }
+    //NOTE Make yo rooms here...
+    public void Setup()
+    {
+      throw new System.NotImplementedException();
     }
+  }
 }
